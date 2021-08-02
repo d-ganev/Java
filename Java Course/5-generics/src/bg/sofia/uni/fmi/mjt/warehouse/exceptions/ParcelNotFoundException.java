7bb844e389b8c4f4651947fabe10c414abd23724
d-ganev <79646050+d-ganev@users.noreply.github.com>
2021-08-02 18:10:48 +0300

    Add 5-generics

---
+++ Java Course/5-generics/src/bg/sofia/uni/fmi/mjt/warehouse/exceptions/ParcelNotFoundException.java
@@ -0,0 +1,8 @@
+package bg.sofia.uni.fmi.mjt.warehouse.exceptions;
+
+public class ParcelNotFoundException extends RuntimeException {
+
+    public ParcelNotFoundException(String message) {
+        super(message);
+    }
+}
