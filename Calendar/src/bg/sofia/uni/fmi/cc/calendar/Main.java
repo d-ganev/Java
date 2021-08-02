8a464c89cabf68615de956aba86fa4e2efb568af
d-ganev <79646050+d-ganev@users.noreply.github.com>
2021-08-02 21:55:03 +0300

    Delete Calendar/src/bg/sofia/uni/fmi/cc/calendar directory

--- Calendar/src/bg/sofia/uni/fmi/cc/calendar/Main.java
+++
@@ -1,9 +0,0 @@
-
-package bg.sofia.uni.fmi.cc.calendar;
-
-public class Main {
-    public static void main(String[] args) {
-        AnnualCalendar annualCalendar = new AnnualCalendar();
-        annualCalendar.printCalendar();
-    }
-}
