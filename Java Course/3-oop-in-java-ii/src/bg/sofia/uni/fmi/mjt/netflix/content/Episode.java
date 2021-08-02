6a3b998e429b4f1281dea58030ca696eb331105f
d-ganev <79646050+d-ganev@users.noreply.github.com>
2021-08-02 18:06:40 +0300

    Add 3-oop-in-java-ii

---
+++ Java Course/3-oop-in-java-ii/src/bg/sofia/uni/fmi/mjt/netflix/content/Episode.java
@@ -0,0 +1,25 @@
+
+package bg.sofia.uni.fmi.mjt.netflix.content;
+
+public record Episode(String name, int duration) {
+}
+
+/*
+public class Episode {
+    private String name;
+    private int duration;
+
+    public Episode(String name, int duration) {
+        this.name = name;
+        this.duration = duration;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+*/
---
+++ Java Course/3-oop-in-java-ii/src/bg/sofia/uni/fmi/mjt/netflix/platform/StreamingService.java
@@ -0,0 +1,17 @@
+package bg.sofia.uni.fmi.mjt.netflix.platform;
+
+import bg.sofia.uni.fmi.mjt.netflix.account.Account;
+import bg.sofia.uni.fmi.mjt.netflix.content.Streamable;
+import bg.sofia.uni.fmi.mjt.netflix.exceptions.ContentUnavailableException;
+
+public interface StreamingService {
+
+    void watch(Account user, String videoContentName) throws ContentUnavailableException;
+
+    Streamable findByName(String videoContentName);
+
+    Streamable mostViewed();
+
+    int totalWatchedTimeByUsers();
+
+}
