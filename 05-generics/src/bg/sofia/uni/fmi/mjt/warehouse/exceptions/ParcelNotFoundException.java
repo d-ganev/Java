3d900434f36c74def60f0c69e9403f9adec623d8
d-ganev <79646050+d-ganev@users.noreply.github.com>
2021-08-02 21:43:30 +0300

    Delete 05-generics/src/bg/sofia/uni/fmi/mjt/warehouse directory

--- 05-generics/src/bg/sofia/uni/fmi/mjt/warehouse/exceptions/ParcelNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package bg.sofia.uni.fmi.mjt.warehouse.exceptions;
-
-public class ParcelNotFoundException extends RuntimeException {
-
-    public ParcelNotFoundException(String message) {
-        super(message);
-    }
-}
