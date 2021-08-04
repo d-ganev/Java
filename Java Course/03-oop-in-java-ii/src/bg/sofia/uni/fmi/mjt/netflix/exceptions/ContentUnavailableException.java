8c291592e6a0155711bfa3ca50f4f6559c7d1773
d-ganev <79646050+d-ganev@users.noreply.github.com>
2021-08-04 21:24:39 +0300

    Delete Java Course/03-oop-in-java-ii/src/bg/sofia/uni/fmi/mjt/netflix directory

--- Java Course/03-oop-in-java-ii/src/bg/sofia/uni/fmi/mjt/netflix/exceptions/ContentUnavailableException.java
+++
@@ -1,8 +0,0 @@
-package bg.sofia.uni.fmi.mjt.netflix.exceptions;
-
-public class ContentUnavailableException extends Exception {
-
-    public ContentUnavailableException(String message) {
-        super(message);
-    }
-}
